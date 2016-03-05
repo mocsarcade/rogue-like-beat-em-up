de78770c013d51d53059a37181fc506f8ca50b27
McPherson <mcphandr@amazon.com>
2016-03-05 13:26:22 -0800

    Created a script for sharing your builds.

---
+++ share.sh
@@ -0,0 +1,20 @@
+# To run:
+# ./share.sh v.0.0.1
+
+git clone https://github.com/mocsarcade/enchiridion --branch gh-pages shares
+
+node build --production
+
+mkdir -p ./shares/$1
+cp -r ./builds/web/* ./shares/$1
+
+git --git-dir=./shares/.git --work-tree=./shares add $1
+git --git-dir=./shares/.git --work-tree=./shares commit -m "Pushed $1"
+git --git-dir=./shares/.git --work-tree=./shares push origin gh-pages
+
+rm -rf shares
+
+echo
+echo Share your build by going to:
+echo https://mocsarcade.github.io/enchiridion/$1
+echo
