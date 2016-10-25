e8ca0678a7ac338c10bffb02bf178b7c54b4c645
Will Lewis <will@wflewis.com>
2016-10-25 03:31:24 -0400

    Add the necessary scripts to electronify the game

--- electron-setup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-node build
-cp ./electron-bootstrap.js ./builds/web/electron-bootstrap.js
-cp ./package.json ./builds/web/package.json
-node electronify
--- electronify.js
+++ electronify.js
@@ -1,5 +1,10 @@
+var build = require("./build.js")
 var fs = require("fs")
 var path = require("path")
+var ncp = require("ncp").ncp
+ncp.limit = 16
+ncp("./electron-bootstrap.js", "./builds/web/electron-bootstrap.js")
+ncp("./package.json", "./builds/web/package.json")
 
 var packager = require('electron-packager')
 packager({
