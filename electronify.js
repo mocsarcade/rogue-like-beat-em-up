e5bb4e95182f10a0bbcff4bb3bbe3c790a839c98
Will Lewis <will@wflewis.com>
2016-10-25 03:46:30 -0400

    Clean up electronify

--- electronify.js
+++ electronify.js
@@ -1,9 +1,13 @@
-var build = require("./build.js")
 var fs = require("fs")
 var path = require("path")
 var ncp = require("ncp").ncp
 ncp.limit = 16
-ncp("./electron-bootstrap.js", "./builds/web/electron-bootstrap.js")
+ncp("./electron-bootstrap.js", "./builds/web/electron-bootstrap.js", function(err){
+    if (err) {
+        console.log("Make sure you've built the game before you electronify it.")
+        process.exit()
+    }
+})
 ncp("./package.json", "./builds/web/package.json")
 
 var packager = require('electron-packager')
