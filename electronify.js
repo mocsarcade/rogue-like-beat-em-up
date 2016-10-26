20de3f5637795f65056447ad8ca91d442f3f174c
Will <will.l@delegator.com>
2016-10-26 06:49:42 -0500

    Add electron build script (#54)
    * Add basic electron support
    
    * Add electron packager
    
    * Add the necessary scripts to electronify the game
    
    * Clean up electronify
    
    * Move the output of electronify to builds/electron

---
+++ electronify.js
@@ -0,0 +1,18 @@
+var fs = require("fs")
+var path = require("path")
+var ncp = require("ncp").ncp
+ncp.limit = 16
+ncp("./electron-bootstrap.js", "./builds/web/electron-bootstrap.js", function(err){
+    if (err) {
+        console.log("Make sure you've built the game before you electronify it.")
+        process.exit()
+    }
+})
+ncp("./package.json", "./builds/web/package.json")
+
+var packager = require('electron-packager')
+packager({
+    "dir":"builds/web",
+    "all":true,
+    "out":"builds/electron"
+})
